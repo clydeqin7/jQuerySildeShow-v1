bcd98f06e7206c28a80b865715973e057b963607
alawld <1679748907@qq.com>
2017-12-25 17:16:31 +0800

    优化自动播放代码

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var allButtons = $(" #buttons > span ");
 
-console.dir(allButtons.length);
 for (let i = 0; i < allButtons.length; i++) {
   $(allButtons[i]).on("click", function(x) {
     var index = $(x.currentTarget).index();
@@ -9,29 +8,33 @@ for (let i = 0; i < allButtons.length; i++) {
       transform: "translate(" + p + "px)"
     });
     n = index;
-    allButtons
-      .eq(n)
-      .addClass("red")
-      .siblings(".red")
-      .removeClass("red");
+    activeButton(allButtons.eq(n));
   });
 }
+
 
 var n = 0;
 var size = allButtons.length;
 allButtons.eq(n % size).trigger("click");
-
-var timeId = setInterval(() => {
-  n += 1;
-  allButtons.eq(n % size).trigger("click");
-}, 1000);
+var timeId = setTimer()
 
 $(".window").on("mouseenter", function() {
-  window.clearInterval(timeId);
-});
+  window.clearInterval(timeId)
+})
 $(".window").on("mouseleave", function() {
-  timeId = setInterval(() => {
+  timeId = setTimer()
+})
+
+/* funciton */
+function activeButton($button) {
+  $button
+    .addClass("red")
+    .siblings(".red")
+    .removeClass("red");
+}
+function setTimer() {
+  return setInterval(() => {
     n += 1;
     allButtons.eq(n % size).trigger("click");
   }, 1000);
-});
+}
