51aef98dea69da38a983783b0ae396dfaf66b180
alawld <1679748907@qq.com>
2017-12-25 17:12:17 +0800

    实现自动播放

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-var allButtons = $(' #buttons > span ')
+var allButtons = $(" #buttons > span ");
 
-        console.dir(allButtons.length)
-for(let i=0; i<allButtons.length; i++){
-    $(allButtons[i]).on('click', function(x){
-        var index = $(x.currentTarget).index()
-        var p = index * -300
-        $('#images').css({
-            transform:'translate(' + p +'px)'
-        })
-        n = index 
-        allButtons.eq(n)
-            .addClass('red')
-            .siblings('.red').removeClass('red')
-    })
+console.dir(allButtons.length);
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on("click", function(x) {
+    var index = $(x.currentTarget).index();
+    var p = index * -300;
+    $("#images").css({
+      transform: "translate(" + p + "px)"
+    });
+    n = index;
+    allButtons
+      .eq(n)
+      .addClass("red")
+      .siblings(".red")
+      .removeClass("red");
+  });
 }
+
+var n = 0;
+var size = allButtons.length;
+allButtons.eq(n % size).trigger("click");
+
+var timeId = setInterval(() => {
+  n += 1;
+  allButtons.eq(n % size).trigger("click");
+}, 1000);
+
+$(".window").on("mouseenter", function() {
+  window.clearInterval(timeId);
+});
+$(".window").on("mouseleave", function() {
+  timeId = setInterval(() => {
+    n += 1;
+    allButtons.eq(n % size).trigger("click");
+  }, 1000);
+});
